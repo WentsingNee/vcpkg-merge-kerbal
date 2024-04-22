b9ba08a473d86eec9a25cea22509313852afad19
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-04-22 17:53:22 +0800

    [kerbal] support installation of pretty-printer

--- ports/kerbal/portfile.cmake
+++ ports/kerbal/portfile.cmake
@@ -6,6 +6,32 @@ vcpkg_from_github(
         HEAD_REF main
 )
 
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+        FEATURES
+            "pretty-printer"       WITH_PRETTY_PRINTER
+)
+
+if (WITH_PRETTY_PRINTER)
+    vcpkg_from_github(
+            OUT_SOURCE_PATH SOURCE_PATH_PRETTY_PRINTER
+            REPO WentsingNee/KerbalPrettyPrinter
+            REF "v${VERSION}"
+            SHA512 427ffb87cd8f5428fd68646b9855f453e136d6e8009fe084361137cf291f56ba1f7208e3e5d9b7f82577bf20f382dc3b92897f8fdbeb3e99733e647c3c1a3180
+            HEAD_REF main
+    )
+    file(GLOB pretty_printer_files
+            LIST_DIRECTORIES True
+            "${SOURCE_PATH_PRETTY_PRINTER}/*"
+    )
+    foreach (e IN LISTS pretty_printer_files)
+        file(
+            COPY "${e}"
+            DESTINATION "${SOURCE_PATH}/pretty_printer"
+        )
+    endforeach ()
+endif ()
+
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
 )
